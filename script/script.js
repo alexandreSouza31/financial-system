18ab15f13914f8eac62fe18bfd838969635716a4
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-04-21 22:09:17 -0300

    :recycle: refactor: will refactor the error and success message functions

--- script/script.js
+++ script/script.js
@@ -65,6 +65,8 @@ if (body_login) {//curto-circuito
     }
 
     function setTimeOutDanger() {
+        message.style.display = "block";//fazer no cadastro
+        alert_danger.classList.remove("disabled");
         setTimeout(() => {
             message.style.display = "none";
             alert_danger.classList.add("disabled");
@@ -73,17 +75,21 @@ if (body_login) {//curto-circuito
 
     function setTimeOutSuccess(location) {
         if (location === true) {
+            alert_success.classList.remove("disabled");
+            message.style.display = "block";//fazer no cadastro
             setTimeout(() => {
                 message.style.display = "none";
                 alert_success.classList.add("disabled");
                 return window.location.href = location;
             }, 3000)
         } else {
+            alert_success.classList.remove("disabled");
+            message.style.display = "block";//fazer no cadastro
             setTimeout(() => {
                 message.style.display = "none";
                 alert_success.classList.add("disabled");
                 return window.location.href = "";
-            }, 6000)
+            }, 5000)
         }
     }
 
@@ -94,50 +100,33 @@ if (body_login) {//curto-circuito
     enter_btn.addEventListener("click", (e) => {
         e.preventDefault();
         if (validateLogin() === false) {
-            message.style.display = "block";//fazer no cadastro
-
             alert_danger.innerHTML = `preencha os dados corretamente!`;
-            alert_danger.classList.remove("disabled");
 
             setTimeOutDanger()
             return;
         } else {
 
-
             firebase.auth().signInWithEmailAndPassword(email_login.value, password_login.value).//retorna uma promisse
                 //uma promisse informa que vai retornar, mas não quando,pois é assíncrona.
                 then(response => {//que é a resposta. Então consigo tratá-la.
-                    alert_success.classList.remove("disabled");
-
-                    message.style.display = "block";//fazer no cadastro
+                    
                     alert_success.innerHTML = `Entrando...`;
 
-                    setTimeOutSuccess();
+                    setTimeOutSuccess("/home.html");
                 }).catch(error => {
                     if (error.code == "auth/wrong-password") {
-                        message.style.display = "block";
                         alert_danger.innerHTML = `Senha inválida!`;
-                        alert_danger.classList.remove("disabled");
 
-                        setTimeout(() => {
-                            message.style.display = "none";
-                            alert_danger.classList.add("disabled");
-                        }, 2000)
+                        setTimeOutDanger();
 
                         console.log(error)
                     } else if (error.code == "auth/user-not-found") {
-                        message.style.display = "block";
                         alert_danger.innerHTML = `Usuário não cadastrado!`;
-                        alert_danger.classList.remove("disabled");
 
-                        setTimeout(() => {
-                            message.style.display = "none";
-                            alert_danger.classList.add("disabled");
-                        }, 2000)
+                        setTimeOutDanger();
                     } else {
                         console.log("outro erro");
                     }
-
                 });
         }
     })
@@ -161,16 +150,9 @@ if (body_login) {//curto-circuito
         closeEye.classList.add("disabled");
     })
 
-
     function sendEmailRecovery() {
-        message.style.display = "block";
         alert_success.innerHTML = `caso haja esse email em nosso banco de dados você receberá um link em breve!(verifique a caixa de spam também)`;
-        alert_success.classList.remove("disabled");
-
-        setTimeout(() => {
-            message.style.display = "none";
-            alert_success.classList.add("disabled");
-        }, 7000)
+        setTimeOutSuccess();
     }
 
     function recoveryPassowrd() {
@@ -184,7 +166,7 @@ if (body_login) {//curto-circuito
         });
     }
 
-    function invalidPasswoed() {
+    function invalidPassword() {
         firebase.auth().sendPasswordResetEmail(email_login.value).then(() => {
             if (error.code == "auth/wrong-password") {
                 return "Senha inválida!"
@@ -193,7 +175,7 @@ if (body_login) {//curto-circuito
     }
 
     forgot.addEventListener("click", () => {
-        invalidPasswoed()
+        invalidPassword()
         recoveryPassowrd()
     });
 
