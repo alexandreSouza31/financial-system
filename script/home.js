1fbdc3f3c8983cdd3ffafd2130b6a6eecaca71f2
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-04-25 13:58:05 -0300

    :lipstick: feat: will return validation when adding a new transaction

--- script/home.js
+++ script/home.js
@@ -1,5 +1,7 @@
 const body_home = document.querySelector(".body-home")
 let message = document.querySelector(".message");
+let alert_danger = document.querySelector(".alert-danger");
+let alert_success = document.querySelector(".alert-success");
 
 if (body_home) {
     //configs sessão
@@ -25,10 +27,9 @@ if (body_home) {
         message.style.display = "block";
     }
 
-    firebase.auth().onAuthStateChanged(user => {//matém usuário logado, caso tenha feito login
+    firebase.auth().onAuthStateChanged(user => {//se não tiver logado vai pra tela de login
         if (user) {
-            //console.log(user)
-            // h3.innerHTML=`Olá, ${user}`
+            //window.location.href = "/index.html";
         }
     })
     //fim das configs da sessão
@@ -37,7 +38,10 @@ if (body_home) {
 
     const input_description = document.querySelector(".description");
     const input_amount = document.querySelector(".amount");
-    const input_type = document.querySelector("#type");
+    const input_date = document.querySelector(".date");
+    const input_type = document.querySelector("#type")
+    const opt_include = document.querySelector(".opt-include");
+    const opt_expense = document.querySelector(".opt-expense");
     const include_btn = document.querySelector("#include-btn");
 
     const tbody_income = document.querySelector(".tbody-income");
@@ -54,11 +58,11 @@ if (body_home) {
             findTransactions_expense(user);
             findTransactions_income(user);
             //findUser(user)
-            
+
         }
     })
-    
-    
+
+
     function findTransactions_income(user) {//busca as transações de entrada do usuário
         message.style.display = "block";
         firebase.firestore()
@@ -70,15 +74,15 @@ if (body_home) {
                 console.log(user.uid)
                 message.style.display = "none";
                 const transactions_income = snapshot.docs.map(doc => doc.data())//o doc faz parte do que é retornado do firestore
-                
+
                 addTransactionsIncomeToScreen(transactions_income);
             })
             .catch(error => {
                 message.style.display = "none";
                 //console.log(error)
-        })
+            })
     }
-    
+
 
     function findTransactions_expense(user) {//busca as transações de saída do usuário
         message.style.display = "block";
@@ -92,10 +96,10 @@ if (body_home) {
                 const transactions_expense = snapshot.docs.map(doc => doc.data())
                 console.log(transactions_expense)
                 addTransactionsExpenseToScreen(transactions_expense);
-        })
+            })
     }
 
-    
+
     // function findUser(user) {//busca o usuário
     //     firebase.firestore()
     //         .collection("users")
@@ -104,7 +108,7 @@ if (body_home) {
     //         .then(snapshot => {
     //             const user=snapshot.docs.map(doc => doc.data())
     //             addUserInScreen(user);
-                
+
     //     })
     // }
 
@@ -117,7 +121,7 @@ if (body_home) {
 
     function addTransactionsIncomeToScreen(transactions) {//recebe as transações e adiciona na tela
         transactions.forEach(transaction => {
-            
+
             const tr_income = document.createElement("tr");
             tr_income.classList.add(transaction.type);
             tr_income.innerHTML =
@@ -150,10 +154,62 @@ if (body_home) {
         return new Date(date).toLocaleDateString("pt-br")
     }
 
-    include_btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        
+
+    let type = input_type.addEventListener("change", () => {
+        if (input_type.value == "Entrada") {
+            console.log("transacao de entrada")
+            return
+        }
+        if (input_type.value == "Saída") {
+            console.log("transacao de saida")
+            return
+        }
+        return input_type.value
     })
 
+    function validNew() {
+        if (input_description.value === "" || input_amount.value === "" || input_date.value === "" || input_type.value == "Digite o tipo da transação") {
+            alert_danger.innerHTML = `preencha os dados corretamente!`;
+            setTimeOutDanger()
+            return
+        }
+        else {
+            alert_success.innerHTML = `transação salva com sucesso!`;
+            setTimeOutSuccess();
+            input_description.value = "";
+            input_amount.value = "";
+            input_date.value = "";
+            input_type.value = "Digite o tipo da transação"
 
+        }
+
+
+    }
+
+
+    include_btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        type
+        validNew()
+
+    })
+
+    function setTimeOutDanger() {
+        message.style.display = "block";
+        alert_danger.classList.remove("disabled");
+        setTimeout(() => {
+            message.style.display = "none";
+            alert_danger.classList.add("disabled");
+        }, 2000)
+    }
+
+    function setTimeOutSuccess() {
+        
+            alert_success.classList.remove("disabled");
+            message.style.display = "block";
+            setTimeout(() => {
+                message.style.display = "none";
+                alert_success.classList.add("disabled");
+            }, 2000)
+    }
 }
